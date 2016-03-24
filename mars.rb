e1752706ceafcab6ab4dd31636623829f0cc2cb6
CJ <dennischoi@Denniss-MacBook.local>
2016-03-24 16:25:00 -0400

    Restart Rover class

--- mars.rb
+++ mars.rb
@@ -1,14 +1,45 @@
 
-class Plateau
-  attr_accessor :x_min, :y_min, :x_max, :y_max
-  def initialize(x_min, y_min, x_max, y_max)
-    @x_min = x_min
-    @y_min = y_min
-    @x_max = x_max
-    @y_max = y_max
+class Rover
+  def initialize(x = 0, y = 0, direction = n)
+    @x_coordinate = x
+    @y_coordinate = y
+    @direction = n
   end
 
-  def plateau_size
-    puts "The Plateau has a size of ()#{x_min}, #{y_min}) by (#{x_max}, #{y_max})."
+  def read_instructions
+    puts "Give me some instructions to move the Rover."
+
+    @input = gets.strip
+      @input.each_char do |i|
+        case i
+          when 'l'
+          when 'r'
+          when 'm'
+            move
+            break
+        end
+      end
+
+
+    # read the input
   end
+
+  def turn
+    # 90 degree turn based on input
+  end
+
+  def move
+    case @direction
+      when "n"
+        y += 1
+      when "s"
+        y -= 1
+      when "e"
+        x += 1
+      when "w"
+        x -= 1
+    end
+  end
+    #move 1 space in direction
+
 end
