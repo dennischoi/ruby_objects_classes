0a08c67093bd333f8fc8054266db87974dad0df6
CJ <dennischoi@Denniss-MacBook.local>
2016-03-24 15:00:41 -0400

    Mars Rover - Class

---
+++ mars.rb
@@ -0,0 +1,16 @@
+# 5 inputs    ie.   gets.chomp
+
+
+
+class Rover_one
+  def initialize(location, direction)
+    @location = location
+    @direction = direction
+
+  end
+
+end
+
+
+class Rover_two
+end
